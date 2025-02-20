ef1195eea65ae505be672b5ed47131baff5e2a15
JeanCarlos20-code <jeancarlos272010@gmail.com>
2025-02-20 10:06:58 -0300

    refactor: query refactored

--- sql/queries/order.sql
+++ sql/queries/order.sql
@@ -5,14 +5,4 @@ VALUES (?,?,?,?,?,?,?);
 -- name: ListCategories :many
 SELECT id, price, tax, final_price, issue_date, type_requisition, delete_at 
 FROM orders 
-WHERE delete_at IS NULL
-ORDER BY 
-    CASE WHEN sqlc.arg(sort) = 'asc' THEN id END ASC,
-    CASE WHEN sqlc.arg(sort) = 'desc' THEN id END DESC
-LIMIT 
-    CASE 
-        WHEN sqlc.arg(limit) IS NULL THEN 18446744073709551615 
-        ELSE sqlc.arg(limit) 
-    END
-OFFSET COALESCE(sqlc.arg(offset), 0);
 
